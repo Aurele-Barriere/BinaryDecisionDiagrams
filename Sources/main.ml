c83b1a9056c5bfe54c94d543a6313eebf8695ad2
Jérémy Thibault <jeremy.thibault@ens-rennes.fr>
2016-04-30 13:10:50 +0200

    wrote build

--- Sources/main.ml
+++ Sources/main.ml
@@ -50,3 +50,14 @@ let rec apply (t:tableT) (h:tableH) (op:op) (i1:id) (i2:id) =
   and (var2, low2, high2) = (var t i2, low t i2, high t i2) in
   if var1 != var2 then raise (Exception_Different_Vars (var1, var2))
   else make t h var1 (apply t h op low1 low2) (apply t h op high1 high2) ;;
+
+let rec build (t:tableT) (h:tableH) (p:prop formula) =
+  match p with
+  | False -> 0
+  | True -> 1
+  | Atom (P x) -> make t h x 0 1
+  | Not x -> apply_neg t h (build t h x)
+  | And (x, y) -> apply t h Et (build t h x) (build t h y)
+  | Or (x, y) -> apply t h Ou (build t h x) (build t h y)
+  | Iff (x, y) -> apply t h Equiv (build t h x) (build t h y)
+  | Imp (x, y) -> apply t h Impl (build t h x) (build t h y)
